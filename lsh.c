28edbbdd16ce4235ca42c6424c8a84d161041cb6
AndrewG76 <aggravino@cpp.edu>
2021-12-03 00:37:03 +0000

    Finish read line and loop

--- lsh.c
+++ lsh.c
@@ -13,3 +13,34 @@ int main(int argc, char **argv)
     return EXIT_SUCCESS;
 }
 
+void lsh_loop(void)
+{
+    char *line;
+    char **args;
+    int status;
+
+    do {
+        printf("> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_execute(args);
+
+        free(line);
+        free(args);
+    } while (status);
+}
+
+#define LSH_RL_BUFSIZE 1024
+char *lsh_read_line(void)
+{
+    int bufsize = LSH_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufsize);
+    int c;
+
+    if (!buffer) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
