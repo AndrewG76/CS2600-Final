a3a2a2dd110c686bb569ceb1734790efb0878434
AndrewG76 <aggravino@cpp.edu>
2021-12-03 00:33:35 +0000

    Create lsh.c file and main method

---
+++ lsh.c
@@ -0,0 +1,15 @@
+//Gravino, Andrew
+//CS2600, Final Part 1
+//12-10-21
+
+int main(int argc, char **argv)
+{
+    //Load config files, if any
+
+    //Run command loop
+    lsh_loop();
+
+    //Perform any shutdown/cleanup
+    return EXIT_SUCCESS;
+}
+
